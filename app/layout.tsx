d56f8e7f659c649d298e429a81de575d13e457f3
Berkan Kılıç <hello@berkankilic.dev>
2023-06-09 03:20:25 +0300

    font weightler eklendi.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import "@/styles/reset.css";
+import "@/styles/global.css";
+import "@/styles/custom.css";
+
 import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
+// font
+import { Inter } from "next/font/google";
+const inter = Inter({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Berkan Kılıç - Jr. Fullstack Web Developer",

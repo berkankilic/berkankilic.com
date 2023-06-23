3c02a120cb026d555de3bc0bce971bf13583240f
Berkan Kılıç <hello@berkankilic.dev>
2023-06-23 11:29:44 +0300

    Header, Main, Services, Skills ve Footer eklendi.

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,26 @@
-
-import Links from "./components/Links";
-import Description from "./components/description";
-import Image from 'next/image';
+import Header from "@/sections/header";
+import Main from "@/sections/main";
+import Services from "@/sections/services";
+import Skills from "@/sections/skills";
+import Footer from "@/sections/footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8 text-white bg-[#080808]">
-      <div className=' flex flex-row items-center'>
-        <Image
-          src="/1x1profile.jpg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className='rounded-full h-[220px] w-[220px]'
-        />
-        <div className='flex flex-col ml-[40px]'>
-
-          <div className="border-left">
-            <h1 className="font-medium text-[24px] mb-1">Berkan Kılıç</h1>
-            <h2 className="font-normal text-[14px] text-white/75 mb-5">Jr. Fullstack web Developer</h2>
-          </div>
-
-          <Links />
-        </div>
-
-      </div>
-      <Description />
-    </main>
+    <>
+      <Header />
+      <main>
+        <section id="main">
+          <Main />
+        </section>
+        <section id="services">
+          <Services />
+        </section>
+        <div className="divider"></div>
+        <section id="skills">
+          <Skills />
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
